088779e468bd76d2d20f143c2cc3fe1ba4ef6b93
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2020-10-15 08:18:19 +0300

    Исправлен контроллер

---
+++ src/main/java/ru/luxsofttest/controller/FilmController.java
@@ -0,0 +1,49 @@
+package ru.luxsofttest.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.luxsofttest.model.Film;
+import ru.luxsofttest.model.Genre;
+import ru.luxsofttest.service.FilmService;
+
+@RestController
+@RequestMapping(produces  = MediaType.APPLICATION_JSON_VALUE)
+public class FilmController  {
+
+    private final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping(value = "/findAll")
+    public Page<Film> findAll() {
+        return filmService.findAll();
+    }
+
+    @PostMapping(value = "/")
+    public Film saveOrUpdate(Film film) {
+        return filmService.saveOrUpdate(film);
+    }
+    @PostMapping(value = "/")
+    public void delete(Film film) {
+        filmService.delete(film);
+    }
+
+    @GetMapping(value = "/sort/{name}")
+    public Page<Film> findAllByNameSortByName(@PathVariable String name) {
+        return filmService.findAllByNameSortByName(name);
+    }
+
+    @GetMapping("/sort/{name}")
+    public Page<Film> findAllByDirectorSortByName(@PathVariable String name) {
+        return filmService.findAllByDirectorSortByName(name);
+    }
+
+
+    @GetMapping("/sort/{name}")
+    public Page<Film> findAllByGenreSortByName(@PathVariable String name) {
+        return filmService.findAllByGenreSortByName(Genre.valueOf(name));
+    }
+}
